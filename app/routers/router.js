101d17a6884abd6b513f48354f24ab918a899ca8
luiso132413 <loviedog@miumg.edu.gt>
2024-10-24 19:06:54 -0600

    TEST10

--- app/routers/router.js
+++ app/routers/router.js
@@ -6,7 +6,7 @@ const cliente = require('../controllers/cliente.controllers.js');
 
 // Rutas de los clientes
 router.post('/api/cliente/create', cliente.create);
-router.get('/api/cliente/all', cliente.retrieveAll);
+router.get('/api/cliente', cliente.retrieveAll);
 router.get('/api/cliente/onebyid/:id', cliente.getById);
 router.put('/api/cliente/update/:id', cliente.updateById);
 router.delete('/api/cliente/delete/:id', cliente.deleteById);
